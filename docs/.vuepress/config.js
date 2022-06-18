0eab11abce771403de01d4382e187fa58457c3dd
jerryleooo <whilgeek@gmail.com>
2022-06-18 22:52:04 +0800

    initial commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    title: 'Blockchain Analytics',
+    description: 'Just playing around',
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Guide', link: '/guide/' },
+            { text: 'External', link: 'https://google.com' },
+        ],
+        sidebar: [
+            '/',
+            '/guide/',
+            '/features/',
+        ],
+        displayAllHeaders: true
+    }
+}
